96b2af8d2c196db94569b7e79135afadac7681fb
peeper <peeper>
2022-07-21 15:37:44 +0900

    nodemon 추가

--- app/app.js
+++ app/app.js
@@ -5,7 +5,7 @@ app.set("views","./src/views");
 app.set("view engine","ejs");
 
 const home = require("./src/routes/home");
-
+app.use(express.static(`${__dirname}/src/public`));
 app.use("/",home);
 
 module.exports = app;
